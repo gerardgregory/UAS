7e863aa3b6e571001d15a6c8a946d74ad90e279d
gerardgregory <96118168+gerardgregory@users.noreply.github.com>
2021-12-16 23:16:21 +0700

    Update uas_12220095.py

--- uas_12220095.py
+++ uas_12220095.py
@@ -45,7 +45,7 @@ ________________________________________________________________________
 ##Produksi Minyak Mentah Terbesar pada Tahun x
 st.markdown('Produksi Minyak Terbesar pada Tahun: ')
 selectTahun = st.selectbox('Pilih Tahun', selectorTahun)
-selectBanyakNegara = st.slider('Pilih Banyak Negara yang Ingin Ditampilkan: ', value=selectorBesar)
+selectBanyakNegara = st.slider('Pilih Banyak Negara yang Ingin Ditampilkan: ', options=selectorBesar)
 dataB = data[data['tahun'] == selectTahun]
 dataB=dataB.sort_values(["produksi"],ascending=[0])
 dataB=dataB[:selectBanyakNegara]
