c2bb44e9e3ef53297e2164cd558d96ccdf4b8950
gerardgregory <96118168+gerardgregory@users.noreply.github.com>
2021-12-16 22:46:36 +0700

    Update uas_12220095.py

--- uas_12220095.py
+++ uas_12220095.py
@@ -16,7 +16,7 @@ data=pd.merge(dataNegara,data,on='kode_negara')
 #membuat selektor display
 selectorNegara = data['name'].drop_duplicates()
 selectorTahun = data['tahun'].drop_duplicates()
-selectorBesar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1620,50,100]
+selectorBesar = [*range(1, 500, 1)]
 
 #Judul
 st.title('Data Produksi Minyak Dunia')
